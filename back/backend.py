c047a6344430d6f0c47d7d5a440d87906d351ced
Guillermo <reyes.g.r.suarez@gmail.com>
2025-07-02 09:26:34 -0500

    Update backend.py

--- back/backend.py
+++ back/backend.py
@@ -24,7 +24,7 @@ def predict_with_tflite(input_data):
     return output_data[0]
 
 def get_db_connection():
-    return psycopg2.connect('postgresql://db_unfv_ver5_user:rTxeXCWafkztYkNnhrRPZCnBIqATGP1c@dpg-d13fbvk9c44c7399ca1g-a.oregon-postgres.render.com/db_unfv_ver5')
+    return psycopg2.connect('postgresql://db_unfv_ver5_user:rTxeXCWafkztYkNnhrRPZCnBIqATGP1c@dpg-d13fbvk9c44c7399ca1g-a/db_unfv_ver5')
 
 @app.route('/api/home', methods=['GET'])
 def home():
