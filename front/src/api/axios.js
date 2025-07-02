04484c6da1fddecc570c88ad0285d063f208481e
Guillermo <reyes.g.r.suarez@gmail.com>
2025-07-02 09:41:22 -0500

    Update axios.js

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-dec.onrender.com",
+  baseURL: "https://backend-dec.onrender.com/api",
   withCredentials: true,
 });
 
