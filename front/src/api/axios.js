f2202f7947ae1071cd233b1785c131db4ea8ef7e
Guillermo <reyes.g.r.suarez@gmail.com>
2025-07-02 09:57:02 -0500

    Update axios.js

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-dec.onrender.com/api"
+  baseURL: "https://backend-dec.onrender.com/api",
 });
 
 export default api;
